2f7a48b11a73741b239fefa85e44a54a8187b88a
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2020-04-09 09:46:25 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -13,41 +13,27 @@ function controleer() {
     if (vraag1 == vragen[0]) {
         document.getElementById("ans1");
         punten += 1;
-    } else {
-        document.getElementById("ans1");
     }
-
     if (vraag2 == vragen[1]) {
         document.getElementById("ans2");
         punten += 1;
-    } else {
-        document.getElementById("ans2");
     }
 
     if (vraag3 == vragen[2]) {
         document.getElementById("ans3");
         punten += 1;
-    } else {
-        document.getElementById("ans3");
     }
-
     if (auto.includes(vraag4)) {
         document.getElementById("ans4");
         punten += 1;
-    } else {
-        document.getElementById("ans4");
     }
 
     if (wadden.includes(vraag5)) {
         document.getElementById("ans5");
         punten += 1;
-    } else {
-        document.getElementById("ans5");
     }
 
     if (punten == 5) {
         document.getElementById("goedeantwoorden").innerHTML = "Alle antwoorden zijn correct!";
-    } else {
-        document.getElementById("goedeantwoorden").innerHTML = punten + " van de vijf antwoorden zijn goed!";
-    }
+    } 
 }
